6cc776042030dfefb78cbd9297622ac75b5773ac
Jonathan Santos <jgs@cin.ufpe.br>
2018-06-05 00:47:01 -0300

    Login com Spotify

--- app/src/main/java/com/if1001/cin/dage/MainActivity.kt
+++ app/src/main/java/com/if1001/cin/dage/MainActivity.kt
@@ -1,22 +1,73 @@
 package com.if1001.cin.dage
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import com.eqot.fontawesome.FontAwesome
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 1337
+    private lateinit var CLIENT_ID: String
+    private lateinit var REDIRECT_URI: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FontAwesome.applyToAllViews(this, findViewById(R.id.button_login_spotify))
+
         button_login_spotify.setOnClickListener(View.OnClickListener {
-            Log.d("login_debug: ", "click login")
-            val i = Intent(applicationContext, MenuActivity::class.java)
-            applicationContext.startActivity(i)
+            ActivityCompat.requestPermissions(this as Activity, arrayOf(Manifest.permission.INTERNET), 1)
+
+            // Get info from strings
+            CLIENT_ID = getString(R.string.spotify_client_id)
+            REDIRECT_URI = "${getString(R.string.com_spotify_sdk_redirect_scheme)}://${getString(R.string.com_spotify_sdk_redirect_host)}"
+
+            Log.d("ClientId", CLIENT_ID)
+            Log.d("Redirect", REDIRECT_URI)
+            var builder: AuthenticationRequest.Builder =
+                    AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+
+            builder.setScopes(arrayOf("user-read-private", "user-read-email", "playlist-read-private", "streaming"))
+            var request: AuthenticationRequest = builder.build()
+
+            AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request)
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            var response = AuthenticationClient.getResponse(resultCode, intent)
+
+            when (response.type) {
+            // Response was successful and contains auth token
+                AuthenticationResponse.Type.TOKEN -> {
+                    Log.d("Token", response.accessToken)
+                    var i = Intent(applicationContext, MenuActivity::class.java)
+                    i.putExtra("ACCESS_TOKEN", response.accessToken)
+                    applicationContext.startActivity(i)
+                }
+
+            // Auth flow returned an error
+                AuthenticationResponse.Type.ERROR -> {
+                    Log.d("Error", response.error)
+                }
+            }// Handle successful response
+            // Handle error response
+            // Most likely auth flow was cancelled
+            // Handle other cases
+        }
     }
 }
--- app/src/main/java/com/if1001/cin/dage/MenuActivity.kt
+++ app/src/main/java/com/if1001/cin/dage/MenuActivity.kt
@@ -1,27 +1,53 @@
 package com.if1001.cin.dage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.if1001.cin.dage.fragments.HomeFragment
 import com.if1001.cin.dage.fragments.PastWorkoutsFragment
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_menu.*
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+
 
 class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var homeFragment: HomeFragment
     private lateinit var pastWorkoutsFragment: PastWorkoutsFragment
+    private lateinit var CLIENT_ID: String
+    private lateinit var userToken: String
+
+    val AUTH_TOKEN_REQUEST_CODE = 0x10
+
+    private val mOkHttpClient = OkHttpClient()
+    private var mCall: Call? = null
 
     private val HOME_FRAGMENT_TAG = "homeTag"
     private val PAST_WORKOUTS_TAG = "pastWorkouts"
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         setSupportActionBar(app_toolbar)
+
+        // Get info from strings
+        CLIENT_ID = getString(R.string.spotify_client_id)
+
+        userToken = intent.getStringExtra("ACCESS_TOKEN")
+        getUserProfile()
+        //var playerConfig = Config(this, userToken, CLIENT_ID)
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, app_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -49,9 +75,9 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (item.itemId) {
             R.id.nav_home -> {
                 val fragment = fragmentManager.findFragmentByTag(HOME_FRAGMENT_TAG)
-                if (fragment == null){
+                if (fragment == null) {
                     supportFragmentManager.beginTransaction().replace(R.id.fragment_holder, this.homeFragment, HOME_FRAGMENT_TAG).commit()
-                }else if (fragment.isHidden){
+                } else if (fragment.isHidden) {
                     supportFragmentManager.beginTransaction().replace(R.id.fragment_holder, this.homeFragment, HOME_FRAGMENT_TAG).commit()
                 }
             }
@@ -60,9 +86,9 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             R.id.nav_past -> {
                 val fragment = fragmentManager.findFragmentByTag(PAST_WORKOUTS_TAG)
-                if (fragment == null){
+                if (fragment == null) {
                     supportFragmentManager.beginTransaction().replace(R.id.fragment_holder, this.pastWorkoutsFragment, PAST_WORKOUTS_TAG).commit()
-                }else if (fragment.isHidden){
+                } else if (fragment.isHidden) {
                     supportFragmentManager.beginTransaction().replace(R.id.fragment_holder, this.pastWorkoutsFragment, PAST_WORKOUTS_TAG).commit()
                 }
             }
@@ -70,5 +96,69 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onDestroy() {
+        cancelCall()
+        super.onDestroy()
+    }
+
+    fun getUserProfile() {
+        val request = Request.Builder()
+                .url("https://api.spotify.com/v1/me")
+                .addHeader("Authorization", "Bearer $userToken")
+                .build()
+
+        cancelCall()
+        mCall = mOkHttpClient.newCall(request)
+
+        mCall?.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("Request", "Failed to fetch data: $e")
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val jsonObject = JSONObject(response.body()?.string())
+                    Log.d("Response", jsonObject.toString())
+
+                    // UI changes have to be done into UI Thread
+                    runOnUiThread {
+                        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+                        val hView = navigationView.inflateHeaderView(R.layout.nav_header_menu)
+
+                        val imageView = hView.findViewById(R.id.imageView) as ImageView
+                        val tv = hView.findViewById(R.id.user_name) as TextView
+
+                        Picasso.get().load(jsonObject.getJSONArray("images").getJSONObject(0).getString("url")).into(imageView)
+                        tv.setText(jsonObject.getString("display_name"))
+                    }
+                } catch (e: JSONException) {
+                    Log.d("Request", "Failed to parse data: $e")
+                }
+            }
+        })
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response = AuthenticationClient.getResponse(resultCode, data)
+
+        if (AUTH_TOKEN_REQUEST_CODE === requestCode) {
+            userToken = response.accessToken
+        }
+    }
+
+    private fun cancelCall() {
+        mCall?.cancel()
+    }
+
+    private fun getRedirectUri(): Uri {
+        return Uri.Builder()
+                .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
+                .authority(getString(R.string.com_spotify_sdk_redirect_host))
+                .build()
     }
 }
