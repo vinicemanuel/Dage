5bb66937c116a380a2e4dcb794de08a01b05ed9d
vinicius emanuel <vinicius.emanuel@muchmore.digital>
2018-06-13 10:22:02 -0300

    basico para fazer a permanencia dos treinos

--- app/src/main/java/com/if1001/cin/dage/model/Workout.kt
+++ app/src/main/java/com/if1001/cin/dage/model/Workout.kt
@@ -1,6 +1,17 @@
 package com.if1001.cin.dage.model
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import android.graphics.PointF
 
-data class Workout(var locationName: String, var routes: List<PointF>, var id: String) {
+const val WORKOUT_TABLE_NAME = "workout"
+const val WORKOUT_NAME_ROW = "name"
+const val WORKOUT_ID_ROW = "id"
+const val WORKOUT_ROUTE = "route"
+
+@Entity(tableName = WORKOUT_TABLE_NAME)
+data class Workout(@ColumnInfo(name = WORKOUT_NAME_ROW) var locationName: String,
+                   var route: List<PointF>,
+                   @ColumnInfo(name = WORKOUT_ID_ROW) @PrimaryKey var id: String) {
 }
