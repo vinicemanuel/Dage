89778f9f17c66fd11be40896bc93edaad2f38183
vinicius emanuel <vinicius.emanuel@muchmore.digital>
2018-06-13 15:42:49 -0300

    estrutura pra salvar a lista de rotas

--- app/src/main/java/com/if1001/cin/dage/model/Workout.kt
+++ app/src/main/java/com/if1001/cin/dage/model/Workout.kt
@@ -3,7 +3,9 @@ package com.if1001.cin.dage.model
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.TypeConverters
 import android.graphics.PointF
+import com.if1001.cin.dage.ListPointConverter
 
 const val WORKOUT_TABLE_NAME = "workout"
 const val WORKOUT_NAME_ROW = "name"
@@ -12,6 +14,6 @@ const val WORKOUT_ROUTE = "route"
 
 @Entity(tableName = WORKOUT_TABLE_NAME)
 data class Workout(@ColumnInfo(name = WORKOUT_NAME_ROW) var locationName: String,
-                   @ColumnInfo(name = WORKOUT_ROUTE) var route: List<PointF>,
+                   @ColumnInfo(name = WORKOUT_ROUTE) @TypeConverters(ListPointConverter::class) var route: String,
                    @ColumnInfo(name = WORKOUT_ID_ROW) @PrimaryKey var id: String) {
 }
